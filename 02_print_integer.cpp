12cea3f95e952719805112b24446d9ebf7093897
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-07-15 22:40:00 +0530

    Print Integer Enter By User

---
+++ 02_print_integer.cpp
@@ -0,0 +1,12 @@
+// WAP To Print The Number Entered By User...
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << "Enter Number:" << endl;
+    cin >> num;
+    cout << num;
+    return 0;
+}

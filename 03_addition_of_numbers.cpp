6a4c091c9b061870d0d72b2e5cd3446afd560b25
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-07-15 22:42:22 +0530

    Addition Of Numbers

---
+++ 03_addition_of_numbers.cpp
@@ -0,0 +1,14 @@
+// WAP To Add Two Numbers Entered by User...
+
+# include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"Enter the first value:\n"; 
+    cin>>num1;
+    cout<<"Enter the second value:\n"; 
+    cin>>num2;
+    cout<<"The sum is:\n"<<num1+num2;
+    return 0;
+}

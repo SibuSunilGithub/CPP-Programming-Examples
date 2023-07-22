8d7a4f6ed1658e914aa57d320fd456a56e13af59
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-07-22 07:58:47 +0530

    Find area of the rectangle

---
+++ 05_area_of_rectangle.cpp
@@ -0,0 +1,16 @@
+// C++ program to find area of rectangle...
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float h, l, a;
+
+    cout << "Enter your height:" << endl;
+    cin >> h;
+
+    cout << "Enter your length:\n";
+    cin >> l;
+
+    cout << "Area of rectangle is:" << h * l;
+}
